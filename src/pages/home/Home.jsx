3668e2a439e7db69d4e62f3458a4b610e00b4d35
Bankah Anthony Bekoe <107210739+bankah-junior@users.noreply.github.com>
2023-12-18 12:51:04 +0000

    Re-arranged Nav Menu

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -10,60 +10,81 @@ import {
 import { ComputerScience, Forex, School, SoftSkills } from "../../components";
 
 const Home = () => {
-  const [toggleMenu, setToggleMenu] = useState(false)
-  const [schoolTab, setSchoolTab] = useState(false)
-  const [forexTab, setForexTab] = useState(true)
-  const [csTab, setCsTab] = useState(false)
-  const [ssTab, setSsTab] = useState(false)
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [schoolTab, setSchoolTab] = useState(false);
+  const [forexTab, setForexTab] = useState(true);
+  const [csTab, setCsTab] = useState(false);
+  const [ssTab, setSsTab] = useState(false);
 
-  const activeTab = "underline decoration-wavy underline-offset-4"
+  const activeTabL = "underline decoration-wavy underline-offset-4";
+  const activeTabS =
+    "flex flex-row items-center justify-end w-full text-[#ffc451]";
+  const noActiveTabS = "flex flex-row items-center justify-end w-full";
 
   const handleSchool = () => {
-    setSchoolTab(true)
-    setForexTab(false)
-    setCsTab(false)
-    setSsTab(false)
-}
-const handleForex = () => {
-    setSchoolTab(false)
-    setForexTab(true)
-    setCsTab(false)
-    setSsTab(false)
-}
-const handleCs = () => {
-    setSchoolTab(false)
-    setForexTab(false)
-    setCsTab(true)
-    setSsTab(false)
-}
-const handleSs = () => {
-    setSchoolTab(false)
-    setForexTab(false)
-    setCsTab(false)
-    setSsTab(true)
-}
-  
+    setSchoolTab(true);
+    setForexTab(false);
+    setCsTab(false);
+    setSsTab(false);
+    setToggleMenu(false);
+  };
+  const handleForex = () => {
+    setSchoolTab(false);
+    setForexTab(true);
+    setCsTab(false);
+    setSsTab(false);
+    setToggleMenu(false);
+  };
+  const handleCs = () => {
+    setSchoolTab(false);
+    setForexTab(false);
+    setCsTab(true);
+    setSsTab(false);
+    setToggleMenu(false);
+  };
+  const handleSs = () => {
+    setSchoolTab(false);
+    setForexTab(false);
+    setCsTab(false);
+    setSsTab(true);
+    setToggleMenu(false);
+  };
+
   return (
     <>
       <nav className="flex items-center justify-between text-[#ffc451] bg-[rgba(0,0,0,0.8)] sm:text-lg">
         <ul className="hidden w-1/3 sm:flex justify-evenly">
           <li>
-            <button className={!forexTab ? null : activeTab} onClick={handleForex}>Forex</button>
-            </li>
+            <button
+              className={!forexTab ? null : activeTabL}
+              onClick={handleForex}
+            >
+              Forex
+            </button>
+          </li>
           <li>
-            <button className={!csTab ? null : activeTab} onClick={handleCs}>Computer Science</button>
-            </li>
+            <button className={!csTab ? null : activeTabL} onClick={handleCs}>
+              Computer Science
+            </button>
+          </li>
         </ul>
         <div className="flex w-1/3 justify-evenly">
           <img src={logo} alt="logo" className="my-1 w-14 h-14 rounded-xl" />
         </div>
         <ul className="hidden w-1/3 sm:flex justify-evenly">
           <li>
-            <button className={!schoolTab ? null : activeTab} onClick={handleSchool}>School</button>
-            </li>
+            <button
+              className={!schoolTab ? null : activeTabL}
+              onClick={handleSchool}
+            >
+              School
+            </button>
+          </li>
           <li>
-            <button className={!ssTab ? null : activeTab} onClick={handleSs}>Soft Skills</button>
-            </li>
+            <button className={!ssTab ? null : activeTabL} onClick={handleSs}>
+              Soft Skills
+            </button>
+          </li>
         </ul>
         <div className="mr-3 sm:hidden">
           {toggleMenu ? (
@@ -84,32 +105,55 @@ const handleSs = () => {
         </div>
       </nav>
       {toggleMenu && (
-        <div className="w-full px-4 pb-2 space-y-5 text-right text-white bg-black rounded-b-lg">
-          <button className="flex flex-row items-center justify-end w-full" onClick={handleSchool}>
-            <span className="pt-4 pb-2 mr-4 scale-in-hor-right">School</span>
-            <AiFillHourglass size={24} color="white" />
-          </button>
-          <hr />
-          <button className="flex flex-row items-center justify-end w-full" onClick={handleForex}>
+        <div className="w-full px-4 pt-4 pb-2 space-y-5 text-right text-white bg-black rounded-b-lg">
+          <button
+            className={!forexTab ? noActiveTabS : activeTabS}
+            onClick={handleForex}
+          >
             <span className="pb-2 mr-4 scale-in-hor-right">Forex</span>
-            <AiOutlineMoneyCollect size={24} color="white" />
+            <AiOutlineMoneyCollect
+              size={24}
+              className={!forexTab ? "white" : "text-[#ffc451]"}
+            />
           </button>
           <hr />
-          <button className="flex flex-row items-center justify-end w-full" onClick={handleCs}>
+          <button
+            className={!csTab ? noActiveTabS : activeTabS}
+            onClick={handleCs}
+          >
             <span className="pb-2 mr-4 scale-in-hor-right">
               Computer Science
             </span>
-            <AiOutlineLaptop size={24} color="white" />
+            <AiOutlineLaptop
+              size={24}
+              className={!csTab ? "white" : "text-[#ffc451]"}
+            />
           </button>
           <hr />
-          <button className="flex flex-row items-center justify-end w-full" onClick={handleSs}>
+          <button
+            className={!schoolTab ? noActiveTabS : activeTabS}
+            onClick={handleSchool}
+          >
+            <span className="pt-4 pb-2 mr-4 scale-in-hor-right">School</span>
+            <AiFillHourglass
+              size={24}
+              className={!schoolTab ? "white" : "text-[#ffc451]"}
+            />
+          </button>
+          <hr />
+          <button
+            className={!ssTab ? noActiveTabS : activeTabS}
+            onClick={handleSs}
+          >
             <span className="pb-2 mr-4 scale-in-hor-right">Soft Skills</span>
-            <AiFillAccountBook size={24} color="white" />
+            <AiFillAccountBook
+              size={24}
+              className={!ssTab ? "white" : "text-[#ffc451]"}
+            />
           </button>
         </div>
       )}
 
-      
       {/* Display Section */}
       <div className="mt-">
         <div className={!schoolTab ? "hidden" : "block"}>
